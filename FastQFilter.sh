d3ccf49185da529cac127bf9ca12f0a76010a50f
slbock <31321436+slbock@users.noreply.github.com>
2018-08-07 14:52:32 -0400

    FastQFilter

--- FastQFilter.sh
+++ FastQFilter.sh
@@ -1,38 +1,39 @@
-python FilterUncorrectablePEfastq.py -1 138599-134_S21_L001_R1_001.cor.fq -2 138599-134_S21_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_1.out
-python FilterUncorrectablePEfastq.py -1 138599-134_S21_L002_R1_001.cor.fq -2 138599-134_S21_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_2.out
-python FilterUncorrectablePEfastq.py -1 138599-134_S21_L003_R1_001.cor.fq -2 138599-134_S21_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_3.out
-python FilterUncorrectablePEfastq.py -1 138599-134_S21_L004_R1_001.cor.fq -2 138599-134_S21_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-110_S27_L001_R1_001.cor.fq -2 138599-110_S27_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_1.out
-python FilterUncorrectablePEfastq.py -1 138599-110_S27_L002_R1_001.cor.fq -2 138599-110_S27_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_2.out
-python FilterUncorrectablePEfastq.py -1 138599-110_S27_L003_R1_001.cor.fq -2 138599-110_S27_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_3.out
-python FilterUncorrectablePEfastq.py -1 138599-110_S27_L004_R1_001.cor.fq -2 138599-110_S27_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-134_S21_L001_R1_001.cor.fq -2 138599-134_S21_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-134_S21_L002_R1_001.cor.fq -2 138599-134_S21_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-134_S21_L003_R1_001.cor.fq -2 138599-134_S21_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-134_S21_L004_R1_001.cor.fq -2 138599-134_S21_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_134_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-23_S22_L001_R1_001.cor.fq -2 138599-23_S22_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_1.out
-python FilterUncorrectablePEfastq.py -1 138599-23_S22_L002_R1_001.cor.fq -2 138599-23_S22_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_2.out
-python FilterUncorrectablePEfastq.py -1 138599-23_S22_L003_R1_001.cor.fq -2 138599-23_S22_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_3.out
-python FilterUncorrectablePEfastq.py -1 138599-23_S22_L004_R1_001.cor.fq -2 138599-23_S22_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-110_S27_L001_R1_001.cor.fq -2 138599-110_S27_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-110_S27_L002_R1_001.cor.fq -2 138599-110_S27_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-110_S27_L003_R1_001.cor.fq -2 138599-110_S27_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-110_S27_L004_R1_001.cor.fq -2 138599-110_S27_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_110_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-63_S23_L001_R1_001.cor.fq -2 138599-63_S23_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_1.out
-python FilterUncorrectablePEfastq.py -1 138599-63_S23_L002_R1_001.cor.fq -2 138599-63_S23_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_2.out
-python FilterUncorrectablePEfastq.py -1 138599-63_S23_L003_R1_001.cor.fq -2 138599-63_S23_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_3.out
-python FilterUncorrectablePEfastq.py -1 138599-63_S23_L004_R1_001.cor.fq -2 138599-63_S23_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-23_S22_L001_R1_001.cor.fq -2 138599-23_S22_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-23_S22_L002_R1_001.cor.fq -2 138599-23_S22_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-23_S22_L003_R1_001.cor.fq -2 138599-23_S22_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-23_S22_L004_R1_001.cor.fq -2 138599-23_S22_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_23_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-68_S18_L001_R1_001.cor.fq -2 138599-68_S18_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_1.out
-python FilterUncorrectablePEfastq.py -1 138599-68_S18_L002_R1_001.cor.fq -2 138599-68_S18_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_2.out
-python FilterUncorrectablePEfastq.py -1 138599-68_S18_L003_R1_001.cor.fq -2 138599-68_S18_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_3.out
-python FilterUncorrectablePEfastq.py -1 138599-68_S18_L004_R1_001.cor.fq -2 138599-68_S18_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-63_S23_L001_R1_001.cor.fq -2 138599-63_S23_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-63_S23_L002_R1_001.cor.fq -2 138599-63_S23_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-63_S23_L003_R1_001.cor.fq -2 138599-63_S23_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-63_S23_L004_R1_001.cor.fq -2 138599-63_S23_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_63_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-69_S24_L001_R1_001.cor.fq -2 138599-69_S24_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_1.out
-python FilterUncorrectablePEfastq.py -1 138599-69_S24_L002_R1_001.cor.fq -2 138599-69_S24_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_2.out
-python FilterUncorrectablePEfastq.py -1 138599-69_S24_L003_R1_001.cor.fq -2 138599-69_S24_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_3.out
-python FilterUncorrectablePEfastq.py -1 138599-69_S24_L004_R1_001.cor.fq -2 138599-69_S24_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-68_S18_L001_R1_001.cor.fq -2 138599-68_S18_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-68_S18_L002_R1_001.cor.fq -2 138599-68_S18_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-68_S18_L003_R1_001.cor.fq -2 138599-68_S18_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-68_S18_L004_R1_001.cor.fq -2 138599-68_S18_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_68_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-8_S15_L001_R1_001.cor.fq -2 138599-8_S15_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_1.out
-python FilterUncorrectablePEfastq.py -1 138599-8_S15_L002_R1_001.cor.fq -2 138599-8_S15_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_2.out
-python FilterUncorrectablePEfastq.py -1 138599-8_S15_L003_R1_001.cor.fq -2 138599-8_S15_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_3.out
-python FilterUncorrectablePEfastq.py -1 138599-8_S15_L004_R1_001.cor.fq -2 138599-8_S15_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_4.out
+python FilterUncorrectabledPEfastq.py -1 138599-69_S24_L001_R1_001.cor.fq -2 138599-69_S24_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-69_S24_L002_R1_001.cor.fq -2 138599-69_S24_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-69_S24_L003_R1_001.cor.fq -2 138599-69_S24_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-69_S24_L004_R1_001.cor.fq -2 138599-69_S24_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_69_4.out
 
-python FilterUncorrectablePEfastq.py -1 138599-90_S19_L001_R1_001.cor.fq -2 138599-90_S19_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_1.out
-python FilterUncorrectablePEfastq.py -1 138599-90_S19_L002_R1_001.cor.fq -2 138599-90_S19_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_2.out
-python FilterUncorrectablePEfastq.py -1 138599-90_S19_L003_R1_001.cor.fq -2 138599-90_S19_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-8_S15_L001_R1_001.cor.fq -2 138599-8_S15_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-8_S15_L002_R1_001.cor.fq -2 138599-8_S15_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-8_S15_L003_R1_001.cor.fq -2 138599-8_S15_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_3.out
+python FilterUncorrectabledPEfastq.py -1 138599-8_S15_L004_R1_001.cor.fq -2 138599-8_S15_L004_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_8_4.out
+
+python FilterUncorrectabledPEfastq.py -1 138599-90_S19_L001_R1_001.cor.fq -2 138599-90_S19_L001_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_1.out
+python FilterUncorrectabledPEfastq.py -1 138599-90_S19_L002_R1_001.cor.fq -2 138599-90_S19_L002_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_2.out
+python FilterUncorrectabledPEfastq.py -1 138599-90_S19_L003_R1_001.cor.fq -2 138599-90_S19_L003_R2_001.cor.fq -o fixed 2>&1 > rmunfixable_90_3.out
