69464148b41ca399b689e2293fe2e9b82b664021
slbock <31321436+slbock@users.noreply.github.com>
2018-08-13 15:20:02 -0400

    re-running TrimGalore for 90 L002

--- trimgalore_short.sh
+++ trimgalore_short.sh
@@ -1,5 +1,5 @@
 #PBS -S /bin/bash
-#PBS -N TrimGalore4.0
+#PBS -N TrimGalore5.0
 #PBS -q batch
 #PBS -l nodes=1:ppn=1:AMD
 #PBS -l walltime=12:00:00
@@ -8,4 +8,4 @@
 
 cd /lustre1/sb61937
 module load Trim_Galore/0.4.5-foss-2016b
-trim_galore --fastqc --fastqc_args "--outdir /lustre1/sb61937/TrimGalore_Fastqc/90/ -f fastq" -stringency 3 -o /lustre1/sb61937/TrimGalore_Fastqc/90/ --paired /lustre1/sb61937/rCorrector_Output/fixed_138599-90_S19_L004_R1_001.cor.fq /lustre1/sb61937/rCorrector_Output/fixed_138599-90_S19_L004_R2_001.cor.fq
+trim_galore --fastqc --fastqc_args "--outdir /lustre1/sb61937/TrimGalore_Fastqc/90/ -f fastq" -stringency 3 -o /lustre1/sb61937/TrimGalore_Fastqc/90/ --paired /lustre1/sb61937/rCorrector_Output/fixed_138599-90_S19_L002_R1_001.cor.fq /lustre1/sb61937/rCorrector_Output/fixed_138599-90_S19_L002_R2_001.cor.fq
