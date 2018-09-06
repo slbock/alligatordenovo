7622c3dc56ff220d4598808261f2c40af4e1ecc2
slbock <31321436+slbock@users.noreply.github.com>
2018-09-06 11:09:52 -0400

    max intron length

--- trinity_test.sh
+++ trinity_test.sh
@@ -10,4 +10,4 @@ module load Trinity/2.6.6-foss-2016b
 cd /lustre1/sb61937/STIM_READS_postqc
 
 #run trinity with merged sorted bam file
-Trinity --genome_guided_bam STIM2014.sort.bam --seqType fq --max_memory 100G --CPU 8 --no_version_check --full_cleanup --SS_lib_type RF --output /lustre1/sb61937/TRINITY
+Trinity --genome_guided_bam STIM2014.sort.bam --genome_guided_max_intron 10000 --seqType fq --max_memory 100G --CPU 8 --no_version_check --full_cleanup --SS_lib_type RF --output /lustre1/sb61937/TRINITY
